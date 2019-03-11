036a355b87dc755b2948d11616a2d8fcfd483147
webert3 <webert3@wwu.edu>
2019-03-11 06:52:07 -0700

    Wrong license in setup.py. Specified dependency versions.

--- setup.py
+++ setup.py
@@ -1,25 +1,21 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+REQUIRED_PACKAGES = ["tensorflow==1.12.0", "numpy==1.15.4", "netCDF4==1.4.2"]
 
 setuptools.setup(
     name="deep_climate_emulator",
     version="1.0.0",
-    license="MIT",
+    license="Apache License 2.0",
     author="Ted Weber",
     author_email="weber.ted2@gmail.com",
     description="Residual Network to approximate precipitation fields in "
                 "Earth System Models",
-    long_description=long_description,
+    long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/hutchresearch/deep_climate_emulator",
     packages=['emulator'],
-    install_requires=[
-        'netCDF4',
-        'numpy',
-        'tensorflow'
-    ],
+    install_requires=REQUIRED_PACKAGES,
     include_package_data=True,
     zip_safe=True,
 	classifiers=[
